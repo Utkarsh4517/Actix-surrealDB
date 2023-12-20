059ce8830be0b940761404f87d7d226ca15b965e
utkarsh <uttkarsh.dav@gmail.com>
2023-12-20 19:41:51 +0530

    created pizza struct

--- src/models/pizza.rs
+++ src/models/pizza.rs
@@ -1,15 +1,28 @@
 use serde::{Deserialize, Serialize};
 use validator::Validate;
 
-
 #[derive(Validate, Deserialize, Serialize)]
-pub struct BuyPizzaRequest{
+pub struct BuyPizzaRequest {
     #[validate(length(min = 1, message = "Pizza name required"))]
     pub pizza_name: String,
-
 }
 
 #[derive(Validate, Deserialize, Serialize)]
-pub struct UpdatePizzaURL{
+pub struct UpdatePizzaURL {
     pub uuid: String,
+}
+
+#[derive(Validate, Deserialize, Serialize, Debug)]
+pub struct Pizza {
+    pub uuid: String,
+    pub pizza_name: String,
+}
+
+impl Pizza {
+    pub fn new(uuid: String, pizza_name: String) -> Pizza {
+        Pizza {
+            uuid,
+            pizza_name,
+        }
+    }
 }
