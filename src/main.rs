b4585dd1b3484308ddab36ec990492ceb576a942
utkarsh <utkarsh@debian.debian.com>
2023-12-20 14:11:52 +0530

    created a simple http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, post, patch, App, HttpServer, Responder, HttpResponse};
+
+// endpoints
+#[get("/pizzas")] 
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas available!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new().service(get_pizzas)
+    }).bind("127.0.0.1:8080")?.run().await
 }
