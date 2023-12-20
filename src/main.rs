ed3d7244436e2d0aa0765f98ee2c44d5584a9f1e
utkarsh <utkarsh@debian.debian.com>
2023-12-20 14:24:34 +0530

    added post and patch endpoints

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,19 @@ async fn get_pizzas() -> impl Responder {
     HttpResponse::Ok().body("Pizzas available!")
 }
 
+#[post("/buypizzas")]
+async fn buy_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("buying a pizza")
+}
+
+#[patch("/updatepizza/{uuid}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("updating a pizza")
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(get_pizzas)
+        App::new().service(get_pizzas).service(buy_pizzas).service(update_pizza)
     }).bind("127.0.0.1:8080")?.run().await
 }
